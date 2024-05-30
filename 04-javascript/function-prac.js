a4f2a29a04de934a260489f598d76627c0854b4a
pouryourlove <lqu134@naver.com>
2024-05-30 20:36:04 +0900

    study:js-pracs added

---
+++ 04-javascript/function-prac.js
@@ -0,0 +1,12 @@
+function multifly(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multifly(3, 7));
+
+const square = function (num) {
+  console.log(num ** 2);
+};
+
+square(4);
+square(11);
