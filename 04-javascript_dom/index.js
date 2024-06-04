8797c18fd40a7c295aeedf52a8cbc356fbf07900
pouryourlove <lqu134@naver.com>
2024-06-04 23:11:43 +0900

    git study: js dom-index in process

--- 04-javascript_dom/index.js
+++ 04-javascript_dom/index.js
@@ -7,19 +7,19 @@
 let num = 1;
 let num2 = num;
 
-console.log(num,num2);
-console.log(num === num2);//true
+console.log(num, num2);
+console.log(num === num2); //true
 
 num = 5; // 1->5
-console.log(num,num2); //5 1
-console.log(num === num2);//false
+console.log(num, num2); //5 1
+console.log(num === num2); //false
 
 function changeValue(x) {
-    x = 10;
-    console.log(x); //10
+  x = 10;
+  console.log(x); //10
 }
 
-changeValue(num)
+changeValue(num);
 console.log(num); //5
 
 // 함수로 전달될 때, 변수의 값이 복사되어 함수의 매개변수로 전달 .
@@ -28,9 +28,18 @@ console.log(num); //5
 //x가 num의 값의 복사본을 가지고 있기 때문.
 
 //#2. pass by reference(참조에 의한 전달)
-//-객체나 배열 같은 참조 타입 전달될 때 사용. 
+//-객체나 배열 같은 참조 타입 전달될 때 사용.
 //변수의 메모리 위치(참조)가 전달되므로, 함수 내에서 요소를 변경하면 원래 변수도 변경된다.
 
+const obj = { one: 1, two: 2 };
+const obj2 = obj;
+console.log(obj, obj2); // {one: 1, two:2}
+console.log(obj === obj2); //true
 
+obj.five = 5;
+console.log(obj, obj2); //{one: 1, two:2, five: 5}
+console.log(obj === obj2); //true
 
+//obj와 obj2는 현재 데이터도 같고 참조값 (address,주소)도 같다.
 
+console.log("--------------------------");
