44abe415aae238dda7d091d1d41e697d86a75a00
pouryourlove <lqu134@naver.com>
2024-06-14 03:04:47 +0900

    study:class prac done

--- 12-js-advanced/prac.js
+++ 12-js-advanced/prac.js
@@ -3,37 +3,52 @@
 
 // console.log([...word1,...word2])
 
-class Shape{
-    constructor(width,length){
-        this.width = width;
-        this.length = length;
-    }
-    getArea(){
-        return this.width * this.length
-    }
+class Shape {
+  constructor(width, length) {
+    this.width = width;
+    this.length = length;
+  }
+  getArea() {
+    return this.width * this.length;
+  }
 }
 
-let rec1 = new Shape(3,4)
-console.log(rec1.getArea())
+let rec1 = new Shape(3, 4);
+console.log(rec1.getArea());
 
- class rectangle extends Shape{
-    
-    constructor(width, length, diao){
-        super(width,length)
-        this.diao = diao;
-    } 
-    getdiao(){
-        return Math.sqrt(this.width * this.width + this.length * this.length)
-    }
- }
+class Rectangle extends Shape {
+  constructor(width, length) {
+    super(width, length);
+  }
 
-
-const triangle extends Shape{
-    getArea(){
-        return (this.width * this.length) /2
-    }
+  getDiagonal() {
+    return Math.sqrt(this.width * this.width + this.length * this.length);
+  }
 }
 
-const circle extends Shape {
+let rectangle = new Rectangle(4, 4);
+console.log(rectangle.getDiagonal());
 
+class Triangle extends Shape {
+  constructor(width, length) {
+    super(width, length);
+  }
+  getArea() {
+    return (this.width * this.length) / 2;
+  }
 }
+
+let triangle = new Triangle(6, 6);
+console.log(triangle.getArea());
+
+class Circle extends Shape {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  getArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+let circle = new Circle(10);
+console.log(circle.getArea());
