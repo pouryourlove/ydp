a2a40812357b545233eae969a0c75b45e6d7ac25
pouryourlove <lqu134@naver.com>
2024-06-17 16:15:43 +0900

    study:ajax

---
+++ 16-form-dynamic/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("dynamic", { title: "동적 폼을 배워보자!" });
+});
+
+//ajax
+app.get("/ajax", (req, res) => {
+  //GET 방식이므로 브라우저에서 URL 쿼리까지 직접 입력해서도 값을 확인 가능
+  // http://localhost:8000/ajax?name=션&gender=남자
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post("/ajax", (req, res) => {
+  console.log(req.body); // parser없으면 body 안보인다.
+  res.send(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log("server is running on port 8000");
+});
