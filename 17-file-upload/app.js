69339b7acf2e023b92fcf142268c57cb0f64c145
pouryourlove <lqu134@naver.com>
2024-06-17 18:58:59 +0900

    file upload index.ejs

---
+++ 17-file-upload/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const multer = require("multer"); //multer 모듈 불러오기
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//multer 미들웨어 등록
+app.use("/uploads", express.static(__dirname + "/uploads"));
+//__dirname : 현재위치
+const upload = multer({
+  dest: "upload/",
+});
+
+app.get("/", function (req, res) {
+  res.render("index", { title: "파일 업로드를 배워보자" });
+});
+
+app.listen(PORT, function () {
+  console.log(`http://localhost:${PORT}`);
+});
