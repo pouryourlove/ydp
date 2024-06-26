c121f4a3caf5702aa2a83102a9017731839d0402
pouryourlove <lqu134@naver.com>
2024-06-26 18:42:30 +0900

    delete and update

--- 21-mvc_mysql/controller/Cvisitor.js
+++ 21-mvc_mysql/controller/Cvisitor.js
@@ -23,6 +23,13 @@ exports.getVisitors = (req, res) => {
   })
 };
 
+exports.getVisitor = (req, res) => {
+  // req.params.id: // 조회해야할 id
+  Visitor.getVisitor(req.params.id, (result) => {
+    res.send(result);
+  }); 
+}
+
 exports.postVisitor = (req, res) => {
   console.log(req.body);
 
@@ -35,4 +42,25 @@ exports.postVisitor = (req, res) => {
       name: req.body.name, 
       comment: req.body.comment})
   });
+}
+
+exports.deleteVisitor = (req, res) => {
+  console.log(req.body);
+
+  Visitor.deleteVisitor(req.body.id, (result) => {
+    console.log('controller/CVisitor.js >> ', result);
+    
+    res.send({ result }); // { result: result }
+  })
+}
+
+
+exports.patchVisitor = (req, res) => {
+  console.log(req.body);
+
+  Visitor.patchVisitor(req.body, (result) => {
+    console.log('controller/CVisitor.js >> ', result);
+
+    res.send({ result }); // { result: result }
+  })
 }
--- 21-mvc_mysql/routes/index.js
+++ 21-mvc_mysql/routes/index.js
@@ -9,9 +9,18 @@ const router = express.Router();
 router.get('/', controller.main);
 router.get('/visitors',controller.getVisitors); // 전체 조회 
 
-//post/ visitor
+// get/visitor/:id
+router.get('/visitor/:id', controller.getVisitor); // 하나 조회
+
+
+//post/visitor
 router.post('/visitor', controller.postVisitor); //하나 추가
 
+//delete/visitor
+router.delete('/visitor', controller.deleteVisitor) // 하나 삭제
+
+//patch/visitor
+router.patch('/visitor',controller.patchVisitor); // 하나 수정
 
 
 module.exports = router;
