2862ab9bc89744f8f0081f97b25704b160f38298
pouryourlove <lqu134@naver.com>
2024-06-29 13:57:01 +0900

    study:prac4 done

--- 13-async/prac4.js
+++ 13-async/prac4.js
@@ -1,47 +1,24 @@
-let product, price;
-
-function goMart() {
-  console.log(`마트에 가서 어떤 음료를 살지 고민한다..`);
-}
-
-function pickDrink() {
-  return new Promise((resolve) => {
+function backgroundColor(color) {
+  return new Promise((resolve, reject) => {
     setTimeout(function () {
-        console.log(`고민 끝`);
-        product = `제로콜라`;
-        price = `3000원`;
-        resolve({product, price})
-      }, 3000);
-      
-  })  
- 
-}
-
-function pay(product, price) {
-    return new Promise((resolve) => {
-        setTimeout(function () {
-        console.log(`상품명: ${product} // 가격: ${price}`);
-        resolve()
-          }, 5000);
-         
-    })
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, 1000);
+   
+  })
   
 }
 
-async function exec(){
-    
-    goMart();
-    const {product, price} = await pickDrink();
-    await pay(product, price);
-
+async function color() {
+  try {
+    await backgroundColor('red')
+    await backgroundColor('orange')
+    await backgroundColor('yellow')
+    await backgroundColor('green')
+    await backgroundColor('blue')
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-exec();
-
-
-
-/*
-마트에 가서 어떤 음료를 살지 고민한다..
-고민 끝
-상품명: undefined // 가격: undefined
-*/
+color()
